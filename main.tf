5b8bfdd4398658f183370850bcd2034ccf2fd126
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-09-07 14:38:02 -1000

    Updates to minimize charges

--- main.tf
+++ main.tf
@@ -7,14 +7,30 @@ resource "google_cloud_run_service" "default" {
       containers {
         image = var.image
         resources {
+          # Minimal CPU and memory limits for cost savings
           limits = {
-            memory = var.memory
-            cpu    = var.cpu
+            memory = var.memory      # Set to 128Mi for minimal cost
+            cpu    = var.cpu         # Set to 0.08 for minimal cost
           }
         }
       }
     }
   }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  autogenerate_revision_name = true
+}
+
+# Scale down to zero instances when there are no requests
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = var.region
+  project     = var.project_id
+  service     = google_cloud_run_service.default.name
+  policy_data = google_iam_policy.noauth.policy_data
 }
 
 output "cloud_run_url" {
