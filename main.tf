b6e80b64e27b6a318d0dee09b739200bbedd4f61
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-11-29 08:37:27 -1000

    Change to use the project id to identify the artifact registry

--- main.tf
+++ main.tf
@@ -21,11 +21,11 @@ resource "google_project_iam_member" "grant_gcr_access" {
   member  = "serviceAccount:${google_service_account.cloud_run_sa[0].email}"
 }
 
-# Grant Access to Artifact Registry
+# Grant Artifact Registry access to the Service Account
 resource "google_artifact_registry_repository_iam_member" "grant_artifact_access" {
   count       = var.service_account_email == null ? 1 : 0
+  project     = var.project_id
   repository  = var.artifact_registry_repo_name
-  location    = var.artifact_registry_repo_location
   role        = "roles/artifactregistry.reader"
   member      = "serviceAccount:${google_service_account.cloud_run_sa[0].email}"
 }
