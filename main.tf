710c075cf8ea1ce234e2b7ff1e9d06a0ea5ec15a
Derrin Chong <derrinchong@Derrins-Mac-Studio.local>
2024-11-29 14:04:59 -1000

    Now accept the version of the secret as input

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "google_artifact_registry_repository_iam_member" "grant_artifact_access
   member      = "serviceAccount:${google_service_account.cloud_run_sa[0].email}"
 }
 
+# Cloud Run Service
 resource "google_cloud_run_service" "default" {
   name     = var.app_name
   location = var.region
@@ -63,8 +64,9 @@ resource "google_cloud_run_service" "default" {
             name = env.value.name
             value_from {
               secret_key_ref {
-                name = var.postgres_secret_name
-                key  = env.value.key
+                name    = var.postgres_secret_name
+                key     = env.value.key
+                version = var.secret_version # Use version passed from root module
               }
             }
           }
